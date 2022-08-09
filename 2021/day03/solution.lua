153f4e0a4e40baba2d24bd2d6ca87ea7c24e8399
Felipe Mamud <trovatti@gmail.com>
2022-08-09 21:55:50 +0200

    fixing day 3

--- 2021/day03/solution.lua
+++ 2021/day03/solution.lua
@@ -1,77 +1,91 @@
-function get_data(file)
+function most_common(report, position)
+    return report["0"][position] > report["1"][position] and 0 or 1
+end
+
+function least_common(report, position)
+    return report["0"][position] > report["1"][position] and 1 or 0
+end
+
+function run(file)
+    local report = {
+        ["0"] = {},
+        ["1"] = {}
+    }
+
+    for line in io.lines(file) do
+        local position = 1
+        for digit in line:gmatch("%d") do
+            local bucket = report[digit]
+            bucket[position] = (bucket[position] or 0) + 1
+            position = position + 1
+        end
+    end
+
+    local most, least  = {}, {}
+    for position = 1, #report["0"] do
+        table.insert(most, most_common(report, position))
+        table.insert(least, least_common(report, position))
+    end
+
+    local gamma = tonumber(table.concat(most), 2)
+    local epsilon = tonumber(table.concat(least), 2)
+
+    return gamma * epsilon
+end
+
+function add_filtered(report, filtered, line, position, target, tie)
+    local t = position < #report[tostring(tie)] and target or tie
+    if tonumber(line:sub(position, position)) == t then
+        table.insert(filtered, line)
+    end
+end
+
+function extract(data, fn_common, tie, position)
+    local report = {
+        ["0"] = {},
+        ["1"] = {}
+    }
+
+    for _, line in ipairs(data) do
+        local n = 1
+        for digit in line:gmatch("%d") do
+            local bucket = report[digit]
+            bucket[n] = (bucket[n] or 0) + 1
+            n = n + 1
+        end
+    end
+    
+    local position = position or 1
+
+    if position > data[1]:len() or #data == 1 then
+        return data[1]
+    end
+
+    local target = fn_common(report, position)
+
+    local filtered = {}
+    for _, line in ipairs(data) do
+        add_filtered(report, filtered, line, position, target, tie)
+    end
+
+    if #filtered == 0 and position < #report[tostring(tie)] then
+        table.move(data, 1, #data, 1, filtered)
+    end
+
+    return extract(filtered, fn_common, tie, position + 1)
+end
+
+function run_part2(file)
+
     local data = {}
     for line in io.lines(file) do
         table.insert(data, line)
     end
-    return data
-end
+    
+    local co2 = tonumber(extract(data, least_common, 0), 2)
+    local oxygen = tonumber(extract(data, most_common, 1), 2)
 
-function get_stats(data)
-    local stats = {}
-    for _, value in ipairs(data) do
-        local column = 1
-        for n in value:gmatch("%d") do
-            local container = stats[column] or {}
-            local key = tonumber(n)
-            container[key] = (container[key] or 0) + 1
-            stats[column] = container
-            column = column + 1
-        end
-    end
-    return stats
-end
-
-function get_common(data)
-    local stats = get_stats(data)
-    local most_common, least_common = {}, {}
-    for i = 1, #stats do
-        local column = stats[i]
-        table.insert(most_common, column[0] > column[1] and 0 or 1)
-        table.insert(least_common, column[0] < column[1] and 0 or 1)
-    end
-    return most_common, least_common
-end
-
-function run(file)
-    local data = get_data(file)
-    local most_common, least_common = get_common(data)
-
-    local gama = tonumber(table.concat(most_common), 2)
-    local epsilon = tonumber(table.concat(least_common), 2)
-
-    return gama * epsilon
-end
-
---For example, to determine the oxygen generator rating value using the same example diagnostic report from above:
---
---Start with all 12 numbers and consider only the first bit of each number. There are more 1 bits (7) than 0 bits (5),
---        so keep only the 7 numbers with a 1 in the first position: 11110, 10110, 10111, 10101, 11100, 10000, and 11001.
---Then, consider the second bit of the 7 remaining numbers: there are more 0 bits (4) than 1 bits (3), so keep only the 4 numbers with a 0
---          in the second position: 10110, 10111, 10101, and 10000.
---In the third position, three of the four numbers have a 1, so keep those three: 10110, 10111, and 10101.
---In the fourth position, two of the three numbers have a 1, so keep those two: 10110 and 10111.
---In the fifth position, there are an equal number of 0 bits and 1 bits (one each). So, to find the oxygen generator rating, keep the number with a 1 in that position: 10111.
---As there is only one number left, stop; the oxygen generator rating is 10111, or 23 in decimal.
-
-function get_lsr(data, position)
-    local most_common, least_common = get_common(data)
-
-    local bit = most_common[position]
-
-    local new = {}
-    for _, value in ipairs(data) do
-        if string.sub(value, position, position) == bit then
-            table.insert(new, value)
-        end
-    end
-
-
-end
-
-function run_part2(file)
-    local oxygen, co2 = get_lsr(get_data(file))
-
-    return oxygen * co2
+    return co2 * oxygen
 end
 
 assert(run("sample.txt") == 198)
